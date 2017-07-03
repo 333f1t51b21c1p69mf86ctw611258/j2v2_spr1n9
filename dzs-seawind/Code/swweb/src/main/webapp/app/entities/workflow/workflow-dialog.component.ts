952ef069fb58cff0c41bb6b0b6f02dc682b0a198
Tran Huu Nguyen <nguyenth@dasannetworks.com>
2017-07-03 18:46:48 +0700

    - Remove createdBy, lastModifiedBy, ... on dialog UI. - Set current information for: createdBy, lastModifiedBy, ... fields.

--- dzs-seawind/Code/swweb/src/main/webapp/app/entities/workflow/workflow-dialog.component.ts
+++ dzs-seawind/Code/swweb/src/main/webapp/app/entities/workflow/workflow-dialog.component.ts
@@ -10,6 +10,9 @@ import { Workflow } from './workflow.model';
 import { WorkflowPopupService } from './workflow-popup.service';
 import { WorkflowService } from './workflow.service';
 
+import { Account, Principal } from '../../shared';
+import { User, UserService } from '../../shared';
+
 @Component({
     selector: 'jhi-workflow-dialog',
     templateUrl: './workflow-dialog.component.html'
@@ -22,17 +25,50 @@ export class WorkflowDialogComponent implements OnInit {
     createdDateDp: any;
     lastModifiedDateDp: any;
 
+    account: Account;
+    user: User;
+
     constructor(
         public activeModal: NgbActiveModal,
         private alertService: JhiAlertService,
         private workflowService: WorkflowService,
-        private eventManager: JhiEventManager
+        private eventManager: JhiEventManager,
+
+        private userService: UserService,
+        private principal: Principal
     ) {
+    }
+
+    getCurrentDate() {
+        var dateObj = new Date();
+        var month = dateObj.getUTCMonth() + 1; //months from 1-12
+        var day = dateObj.getUTCDate();
+        var year = dateObj.getUTCFullYear();
+
+        return {year, month, day};
+    }
+
+    initNewWorkflow() {
+        this.workflow.createdDate = this.getCurrentDate();
+        this.workflow.lastModifiedDate = this.workflow.createdDate;
+
+        this.principal.identity().then((account) => {
+            this.account = account;
+
+            this.userService.find(this.account.login).subscribe((user) => {
+                this.user = user;
+
+                this.workflow.createdBy = this.user.id;
+                this.workflow.lastModifiedBy = this.user.id;
+            });
+        });
     }
 
     ngOnInit() {
         this.isSaving = false;
         this.authorities = ['ROLE_USER', 'ROLE_ADMIN'];
+
+        this.initNewWorkflow();
     }
 
     clear() {
